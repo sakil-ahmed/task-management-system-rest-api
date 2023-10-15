30361dcd3ac282e6e725d2600ad78a3ab0c82571
sakil-ahmed <sakilahmed1495@gmail.com>
2023-10-15 13:15:12 +0600

    update

--- src/workspace/schemas/workspace.schema.ts
+++ src/workspace/schemas/workspace.schema.ts
@@ -1,12 +1,12 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
 import { User } from "../../auth/schemas/users.schema";
 
 @Schema({
   timestamps: true,
   versionKey: false
 })
-export class Workspace {
+export class Workspace extends Document {
   @Prop()
   readonly title: string;
 
